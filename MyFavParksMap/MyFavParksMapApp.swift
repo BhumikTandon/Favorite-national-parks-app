bb1386bcaab23ebd4a748119c8361b5ba912ace9
Bhumik Tandon <bhumiktandon.456@gmail.com>
2025-03-18 21:30:04 -0700

    Initial Commit

---
+++ MyFavParksMap/MyFavParksMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavParksMapApp.swift
+//  MyFavParksMap
+//
+//  Created by Bhumik Tandon on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFavParksMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
